dc9644af6ba092c7789db6ae4aee72d25e5e9c25
Jim Lloyd <jim.lloyd@gmail.com>
2023-06-23 15:52:09 -0700

    src/lib/variant.ts

---
+++ src/lib/variant.ts
@@ -0,0 +1,11 @@
+import { Variant } from '@prisma/client'
+import { error } from '@sveltejs/kit'
+
+export function asVariant(v: string): Variant {
+    v = v.toUpperCase();
+    if (v in Variant)
+        return v as Variant
+    else
+        throw error(500, `No such variant: ${v}`)
+
+}
