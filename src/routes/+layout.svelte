9c1e9c05bba2495e7b399d96e7cf37fc4578865e
Jim Lloyd <jim.lloyd@gmail.com>
2023-06-24 10:27:23 -0700

    Full screen, better <nav>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import '@picocss/pico'
+	import type { LayoutServerData } from './$types'
+	export let data: LayoutServerData
 </script>
 
-<div class="container">
-	<nav>
+<div class="container-fluid">
+	<nav class="container-fluid">
 		<ul>
 			<li>
 				<strong>
@@ -11,6 +13,18 @@
 				</strong>
 			</li>
 		</ul>
+		<ul>
+			{#if !data.user}
+				<li><a href="/register">Register</a></li>
+				<li><a href="/login">Login</a></li>
+			{:else}
+				<li>
+					<a href="/logout">Logout</a>
+				</li>
+			{/if}
+		</ul>
 	</nav>
-	<slot />
+	<main class="container-fluid">
+		<slot />
+	</main>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,1 @@
-<script lang="ts">
-	import type { LayoutServerData } from './$types'
-	export let data: LayoutServerData
-</script>
-
-<ul>
-	<form method="POST">
-		<li><a href="/">Home</a></li>
-		{#if !data.user}
-			<li><a href="/register">Register</a></li>
-			<li><a href="/login" role="button">Login</a></li>
-		{:else}
-			<li>
-				<button formaction="/logout" type="submit">Logout</button>
-			</li>
-		{/if}
-	</form>
-</ul>
+<p />
